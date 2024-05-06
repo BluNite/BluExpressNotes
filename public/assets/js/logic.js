5796c5ca62e14ad402aef358d6a53466fd24b7ef
bluNite <ncrowe175@gmail.com>
2024-05-06 09:04:23 -0400

    logic js added class elements to vars typeof logged to check availability after hitting notes path blunite

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -6,8 +6,19 @@ let newNoteBtn;
 let noteList;
 
 
-
-
-
+// if notes path is hit assign variables to class elements
+if (window.location.pathname === '/notes') {
+	noteTitle = document.querySelector('.note-title');
+	noteText = document.querySelector('.note-textarea');
+	saveNoteBtn = document.querySelector('.save-note');
+	newNoteBtn = document.querySelector('.new-note');
+	noteList = document.querySelectorAll('.list-container .list-group');
+}
+// log to check availability of note variables
+console.log(typeof noteTitle);
+console.log(typeof noteText);
+console.log(typeof saveNoteBtn);
+console.log(typeof newNoteBtn);
+console.log(typeof noteList);
 
 
