e97721a995860247922bde9d3f15b290a196acd4
bluNite <ncrowe175@gmail.com>
2024-05-08 08:36:26 -0400

    blunite

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,13 +9,15 @@ const path = require('path');
 
 
 // html/index route
-router.get('/*', (req, res) => {
+router.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 //html/ notes route
-router.get('/notes', (req, res) => {
+router.get('/notes', function (req, res) {
 	res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
+
+
 
 
 
