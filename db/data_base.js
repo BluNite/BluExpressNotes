49f2e9ed4f075f2ed95f0568e66724c3c889c96b
bluNite <ncrowe175@gmail.com>
2024-05-09 01:45:42 -0400

    tracking files blunite

---
+++ db/data_base.js
@@ -0,0 +1,24 @@
+// require dependencies
+const fs = require('fs');
+const util = require('util');
+
+// json database
+const noteData = "/db/db.json";
+
+// read, write file async promise
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class DataBase {
+	async readNotes() {
+		try {
+			const noteRaw = await readFileAsync(noteData, "utf8");
+			return noteRaw ? JSON.parse(noteRaw) : [];
+		} catch (error) {
+			throw error;
+		}
+	}
+}
+
+module.exports = new DataBase();
+
