5b0f7fab5df617107ab0c3296683b4d220eabf65
bluNite <ncrowe175@gmail.com>
2024-05-09 01:47:12 -0400

    adding apiRoutes blunite

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,13 @@
 // require dependencies
 const express = require('express');
-
-
 // express router
 const router = express.Router();
+const DB = require('../db/data_base.js');
 
+router.get("/api/notes", async function (req, res) {
+	const notes = await DB.readNotes();
+	return res.json(notes);
+});
 
 
 
